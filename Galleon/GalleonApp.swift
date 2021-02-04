0447a0bc0d39b52a471c257986b69fb3be64d3c2
Zac Gibson <zgibson@live.com>
2021-02-03 22:43:03 -0600

    Initial Commit

---
+++ Galleon/GalleonApp.swift
@@ -0,0 +1,13 @@
+// Created for Galleon in 2021
+// Using Swift 5.0
+
+import SwiftUI
+
+@main
+struct GalleonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
