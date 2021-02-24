d9cfab78494d97a57aed88784b9fd2512b5b41ea
Zac Gibson <zgibson@live.com>
2021-02-23 22:21:19 -0600

    custom colors in progress bars

--- Galleon/Components /ProgressBar.swift
+++ Galleon/Components /ProgressBar.swift
@@ -6,15 +6,19 @@ import SwiftUI
 struct ProgressBar: View {
     @Binding var value: Float
     
+    var foregroundColor: Color = Color.primary
+    var backgroundColor: Color = Color.secondary
+    
     var body: some View {
         GeometryReader { geometry in
             ZStack(alignment: .leading) {
                 Rectangle().frame(width: geometry.size.width , height: geometry.size.height)
                     .opacity(0.3)
-                    .foregroundColor(Color(UIColor.systemTeal))
+                    .foregroundColor(foregroundColor)
                 
-                Rectangle().frame(width: min(CGFloat(self.value)*geometry.size.width, geometry.size.width), height: geometry.size.height)
-                    .foregroundColor(Color(UIColor.systemBlue))
+                Rectangle()
+                    .frame(width: min(CGFloat(self.value)*geometry.size.width, geometry.size.width), height: geometry.size.height)
+                    .foregroundColor(backgroundColor)
                     .animation(.linear)
             }.cornerRadius(45.0)
         }
